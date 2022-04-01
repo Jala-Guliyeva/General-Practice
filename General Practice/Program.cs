89af63b4aee3d33838de587317cab38c5b5d624d
Jalə Quliyeva <quliyevajale52@gmail.com>
2022-04-01 05:56:24 +0400

    file added

---
+++ General Practice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace General_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            Queue queues = new Queue();
+            queues.Enqueue("Jale");
+            queues.Enqueue("Konul");
+            queues.Enqueue("Ilknur");
+
+            foreach (var item in queues.Reserve(queue))
+            {
+                Console.WriteLine(item);
+            }
+
+
+        }
+    }
+}
---
+++ General Practice/Queue.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace General_Practice
+{
+    internal class Queue
+    {
+        Queue queue;
+        Stack reverse;
+        Queue<string> reversque;
+        public Queue()
+        {
+            queue = new Queue();
+            reverse = new Stack();
+            reversque = new Queue<string>();
+        }
+
+        internal void Enqueue(string v)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Queue<string> Reserve(Queue<String> word)
+        {
+            foreach (var item in word)
+            {
+                reverse.Push(item);
+            }
+
+            foreach (var item in reverse)
+            {
+                reversque.Enqueue((string)item);
+            }
+            return reversque;
+        }
+    }
+}
